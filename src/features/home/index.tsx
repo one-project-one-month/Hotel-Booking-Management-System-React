060b2bfd8394676d0e0848f5b6556805513be290
mgminthant <mterror2004@gmail.com>
2025-05-15 18:28:38 +0630

    Add popular rooms && add rooms

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,6 +1,18 @@
+import {type Room, rooms} from "@/mock/rooms.ts";
+import PopularRoom from "@/features/home/components/PopularRoom.tsx";
 
 export default function Home() {
+
   return (
-    <div>Home Page</div>
+      <>
+          <h1 className={"text-center py-5"}>Home Page</h1>
+           <div className={"container px-4 mx-auto"}>
+                <div className={"grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 lg"}>
+                    {rooms.slice(0,3).map((room: Room,index:number) => (
+                        <PopularRoom room={room} key={index}/>
+                    ))}
+                </div>
+           </div>
+      </>
   )
 }
--- src/features/room/index.tsx
+++ src/features/room/index.tsx
@@ -1,6 +1,17 @@
+import {rooms,type Room} from "@/mock/rooms.ts";
+import Room_ from "@/features/room/components/Room.tsx";
 
 export default function Room() {
   return (
-    <div>Room Page</div>
+      <>
+        <h1 className={"text-center py-5"}>Room Page</h1>
+        <div className={"container mx-auto px-4"}>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto">
+            {rooms.map((room: Room, index: number) => (
+                <Room_ room={room} key={index}/>
+            ))}
+          </div>
+        </div>
+      </>
   )
 }
