d130b2e3cef85ccc8ddf4e240a3783c315849fd0
Aung Khant Paing <akpaing99@gmail.com>
2025-05-21 16:46:51 +0630

    feat:add deluxe and standard rooms section

--- src/features/home/components/PopularRoomCarousel.tsx
+++ src/features/home/components/PopularRoomCarousel.tsx
@@ -13,28 +13,33 @@ import CarouselSkeleton from "./CarouselSkeleton";
 type PopularRoomCardProps = {
   room: Room;
 };
+type CarouselProps = {
+  roomData: Room[];
+  isFeatured: boolean;
+  title: string;
+};
 
-function createRoomResource() {
-  let status = "pending";
-  let result: Room[] | undefined;
-  const suspender = new Promise<void>((resolve) => {
-    setTimeout(async () => {
-      const mod = await import("@/mock/rooms");
-      result = mod.rooms;
-      status = "success";
-      resolve();
-    }, 3000); // 3 second delay
-  });
+// function createRoomResource() {
+//   let status = "pending";
+//   let result: Room[] | undefined;
+//   const suspender = new Promise<void>((resolve) => {
+//     setTimeout(async () => {
+//       const mod = await import("@/mock/rooms");
+//       result = mod.rooms;
+//       status = "success";
+//       resolve();
+//     }, 1000); // 3 second delay
+//   });
 
-  return {
-    read() {
-      if (status === "pending") {
-        throw suspender;
-      }
-      return result!;
-    },
-  };
-}
+//   return {
+//     read() {
+//       if (status === "pending") {
+//         throw suspender;
+//       }
+//       return result!;
+//     },
+//   };
+// }
 
 // Room Card
 function PopularRoomCard({ room }: PopularRoomCardProps) {
@@ -44,42 +49,48 @@ function PopularRoomCard({ room }: PopularRoomCardProps) {
   const [fav, setFav] = useState(false);
 
   return (
-    <div className="relative max-w-60">
+    <div className="relative max-w-60 h-fit">
       <button
         className="absolute right-2 top-2 w-[10%] max-w-8"
         onClick={() => setFav((fav) => !fav)}
       >
         <Heart
-          fill={fav ? "#ea273d" : "#212f3d"}
+          fill={fav ? "#e60076" : "#212f3d"}
           color="#D4D1D0"
           className=" transition-colors"
           strokeWidth={2}
         />
       </button>
-
-      {/* Skeleton loader */}
-      {!imgLoaded && !imgError && (
-        <Skeleton className="w-full h-40 rounded-lg" />
-      )}
-
-      {/* Image or fallback */}
-      {!imgError ? (
-        <img
-          src={imgUrl}
-          alt=""
-          className={`rounded-lg transition-opacity duration-500 ${
-            imgLoaded ? "opacity-100" : "opacity-0 absolute"
-          }`}
-          loading="lazy"
-          onLoad={() => setImgLoaded(true)}
-          onError={() => setImgError(true)}
-        />
-      ) : (
-        <div className="w-full h-40 flex items-center justify-center bg-gray-200 rounded-lg text-gray-500 text-sm">
-          Image unavailable
-        </div>
-      )}
-      <p>{room.type}</p>
+      <div
+        className="w-full h-40 border bg-center bg-no-repeat bg-cover rounded-lg"
+        style={{
+          backgroundImage: imgUrl && !imgError ? `url(${imgUrl})` : undefined,
+        }}
+      >
+        {/* Skeleton loader*/}
+        {!imgLoaded && !imgError && (
+          <div className="absolute inset-0">
+            <Skeleton className="w-full h-full rounded-lg" />
+          </div>
+        )}
+        {/* Hidden img to handle onLoad and onError */}
+        {imgUrl && !imgLoaded && !imgError && (
+          <img
+            src={imgUrl}
+            alt=""
+            className="hidden"
+            onLoad={() => setImgLoaded(true)}
+            onError={() => setImgError(true)}
+          />
+        )}
+        {/* Error msg for the error in the img fetching  */}
+        {imgLoaded && imgError && (
+          <div className="w-full h-full flex items-center justify-center bg-gray-200 rounded-lg text-gray-500 text-sm">
+            Image unavailable
+          </div>
+        )}
+      </div>
+      <p className=" text-lg leading-6">{room.details.title}</p>
       <p className="text-gray-500 text-xs">
         ${room.price} for night,
         {room.guestLimit === 1
@@ -89,25 +100,30 @@ function PopularRoomCard({ room }: PopularRoomCardProps) {
     </div>
   );
 }
-const roomResource = createRoomResource();
+// const roomResource = createRoomResource();
 
-function PopularRoomCarouselContent() {
-  const roomData = roomResource.read();
-  return (
-    <>
-      {roomData.map((room) => (
-        <CarouselItem
-          key={room.roomNo}
-          className="basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/6"
-        >
-          <PopularRoomCard room={room} />
-        </CarouselItem>
-      ))}
-    </>
-  );
-}
+// function PopularRoomCarouselContent() {
+//   const roomData = roomResource.read();
+//   return (
+//     <>
+//       {roomData.map((room) => (
+//         <CarouselItem
+//           key={room.roomNo}
+//           className="basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/6"
+//         >
+//           <PopularRoomCard room={room} />
+//         </CarouselItem>
+//       ))}
+//     </>
+//   );
+// }
+
 //Carousel
-export default function PopularRoomCarousel() {
+export default function RoomCarousel({
+  roomData,
+  isFeatured,
+  title,
+}: CarouselProps) {
   return (
     <>
       <Carousel
@@ -119,13 +135,36 @@ export default function PopularRoomCarousel() {
       >
         <CarouselContent>
           <Suspense fallback={<CarouselSkeleton />}>
-            <PopularRoomCarouselContent />
+            {roomData.map((room: Room) => (
+              <CarouselItem
+                key={room.roomNo}
+                className="basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/6"
+              >
+                <PopularRoomCard room={room} />
+              </CarouselItem>
+            ))}
           </Suspense>
         </CarouselContent>
-        <div className="border absolute -top-6 right-10">
-          <CarouselPrevious className="-left-6" />
-          <CarouselNext className="" />
-        </div>
+
+        <div className="absolute -top-6 text-sm font-bold">{title}</div>
+
+        {isFeatured && (
+          <div className="border absolute -top-6 right-10">
+            <CarouselPrevious className=" cursor-pointer -left-6" />
+            <CarouselNext className="cursor-pointer" />
+          </div>
+        )}
+
+        {!isFeatured && (
+          <div className="absolute -top-6 right-0">
+            <a
+              href=""
+              className=" underline text-pink-600 hover:text-pink-700 focus:text-pink-700 text-xs"
+            >
+              See More...
+            </a>
+          </div>
+        )}
       </Carousel>
     </>
   );
