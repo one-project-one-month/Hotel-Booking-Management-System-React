876e732391d2a0c46a7a17ce5eee76196cffb566
Wunna Aung <94585967+wunnaaung-dev@users.noreply.github.com>
2025-05-30 15:44:00 +0700

    add:footer section (#21)
    Co-authored-by: mgminthant <mterror2004@gmail.com>

--- src/features/roomDetails/components/RoomDetailUi.tsx
+++ src/features/roomDetails/components/RoomDetailUi.tsx
@@ -41,7 +41,7 @@ export default function RoomDetailUi({
         <section className="space-y-4">
           <Picker />
 
-          <Button className="w-full text-lg py-7 rounded-full cursor-pointer shadow-lg bg-rose-500 hover:bg-rose-600 text-white">
+          <Button className="w-full text-lg py-7 rounded-full cursor-pointer shadow-lg bg-rose-400 hover:bg-rose-500 text-white">
             <Link
               to={`/rooms/checkout/${currentRoom.roomNo}`}
               state={{ roomData: currentRoom }}
