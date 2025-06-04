8578937bb9c2afc0df98f37a3e63e574ff5debd5
mgminthant <mterror2004@gmail.com>
2025-06-04 11:23:31 +0630

    add:remove from wishlist

--- src/features/room/components/Room.tsx
+++ src/features/room/components/Room.tsx
@@ -4,10 +4,50 @@ import { Link } from "react-router";
 import type { Room } from "@/types/rooms.ts";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Button } from "@/components/ui/button";
+import { Trash2 } from "lucide-react";
+import {toast} from 'sonner';
 
-export default function Room({ room }: { room: Room }) {
+import { FAVS_KEY } from "@/config/constants";
+
+export default function Room({
+  room,
+  hasWishList,
+  setRooms,
+}: {
+  room: Room;
+  hasWishList: boolean;
+  setRooms: React.Dispatch<React.SetStateAction<Room[]>>;
+}) {
   const [imgLoaded, setImgLoaded] = useState(false);
-  const imgUrl = (typeof room.imgUrl === 'string' ? JSON.parse(room.imgUrl) : room.imgUrl as string[])[0];
+  const imgUrl = (
+    typeof room.imgUrl === "string"
+      ? JSON.parse(room.imgUrl)
+      : (room.imgUrl as string[])
+  )[0];
+
+  const handleRemoveFromWishList = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    const favRooms = localStorage.getItem(FAVS_KEY);
+
+    if (!favRooms) return;
+
+    const wishlist = JSON.parse(favRooms);
+
+    const updatedWishlist = wishlist.filter(
+      (storeRoom: Room) => storeRoom.id !== room.id
+    );
+
+    localStorage.setItem(FAVS_KEY, JSON.stringify(updatedWishlist));
+    setRooms(updatedWishlist);
+    toast.success("Removed from favorites!")
+
+    //clear localstorage
+    if (wishlist.length === 1) {
+      console.log('hit');
+      
+      localStorage.removeItem(FAVS_KEY);
+    }
+  };
 
   return (
     <div className="rounded-2xl overflow-hidden hover:shadow-md">
@@ -17,8 +57,9 @@ export default function Room({ room }: { room: Room }) {
           src={imgUrl}
           alt="Room Image"
           loading="lazy"
-          className={`w-full h-full object-cover transition-opacity duration-500 ${imgLoaded ? "opacity-100" : "opacity-0 absolute"
-            }`}
+          className={`w-full h-full object-cover transition-opacity duration-500 ${
+            imgLoaded ? "opacity-100" : "opacity-0 absolute"
+          }`}
           onLoad={() => setImgLoaded(true)}
         />
         <span
@@ -26,11 +67,18 @@ export default function Room({ room }: { room: Room }) {
         >
           {room.status}
         </span>
+        {hasWishList && (
+          <Trash2
+            fill="red"
+            color="pink"
+            className="absolute top-2 right-2 cursor-pointer text-xs"
+            onClick={handleRemoveFromWishList}
+          />
+        )}
       </div>
       <div className="p-4">
         <h2 className="text-lg font-semibold">{room.type}</h2>
         {/* <h2 className="text-base text-gray-500">{room.roomNo}</h2> */}
-
         <div className="flex justify-between mt-3">
           <div>
             <p className=" text-gray-500 text-sm">Start From</p>
--- src/features/room/index.tsx
+++ src/features/room/index.tsx
@@ -7,9 +7,10 @@ import { filterRoomsByType } from "../home/utils";
 export default function RoomContainer() {
   const { type } = useParams<{ type: string }>();
   const { data: rooms, isLoading } = useFetchRooms();
-  const filteredRoomsByType = type === "Deluxe" || type === "Standard"
-    ? filterRoomsByType(rooms ?? [], type)
-    : rooms ?? [];
+  const filteredRoomsByType =
+    type === "Deluxe" || type === "Standard"
+      ? filterRoomsByType(rooms ?? [], type)
+      : rooms ?? [];
 
   if (isLoading) {
     return (
@@ -21,12 +22,19 @@ export default function RoomContainer() {
   return (
     <>
       <h1 className="text-center py-5 text-2xl font-bold">
-        {type ? `${type.charAt(0).toUpperCase() + type.slice(1)} Rooms` : "All Rooms"}
+        {type
+          ? `${type.charAt(0).toUpperCase() + type.slice(1)} Rooms`
+          : "All Rooms"}
       </h1>
       <div className={"container mx-auto px-4"}>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto">
           {filteredRoomsByType.map((room: RoomProp, index: number) => (
-            <Room room={room} key={index} />
+            <Room
+              room={room}
+              key={index}
+              hasWishList={false}
+              setRooms={() => {}}
+            />
           ))}
         </div>
       </div>
