29dea5700c248419606f66eda3b559ed80c5a58d
Aung Khant Paing <akpaing99@gmail.com>
2025-06-05 10:30:57 +0630

    fix: receipt UI changed

--- src/features/profile/receipt.tsx
+++ src/features/profile/receipt.tsx
@@ -3,7 +3,7 @@ import { useFetchUserById } from "@/api/services/user";
 
 export default function Receipt() {
   const { data: user } = useFetchUserById(
-    "959e8de4-5fb0-4b91-88ce-a0d3dbdf41ee"
+    "0fa05b29-7b9c-415a-ac42-e8a7046459e5"
   );
   const bookingData = user?.bookings ?? [];
   console.log(bookingData);
@@ -12,6 +12,7 @@ export default function Receipt() {
       <h3 className="text-center my-4 font-semibold text-xl">
         Your Booking Receipts
       </h3>
+
       {bookingData.length !== 0 && user && (
         <div className="flex flex-wrap gap-4 justify-center">
           {bookingData.map((_, i) => (
